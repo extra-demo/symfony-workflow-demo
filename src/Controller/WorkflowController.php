2b6b88a7734851258464f1891a537a0be66e23ad
minbaby.zhang <zhangshaomin_1990@126.com>
2019-03-07 13:39:16 +0800

    002

--- src/Controller/WorkflowController.php
+++ src/Controller/WorkflowController.php
@@ -2,11 +2,28 @@
 
 namespace App\Controller;
 
+use App\Repository\PullRequestRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class WorkflowController extends AbstractController
 {
+    /**
+     * @var PullRequestRepository
+     */
+    protected $pullRequestRepository;
+    
+    /**
+     * WorkflowController constructor.
+     * @param PullRequestRepository $pullRequestRepository
+     */
+    public function __construct(PullRequestRepository $pullRequestRepository)
+    {
+        $this->pullRequestRepository = $pullRequestRepository;
+    }
+    
     /**
      * @Route("/workflow", name="workflow")
      */
@@ -15,5 +32,13 @@ class WorkflowController extends AbstractController
         return $this->render('workflow/index.html.twig', [
             'controller_name' => 'WorkflowController',
         ]);
+    }
+    
+    /**
+     * @Route("/workflow/incr", name="incr")
+     */
+    public function incr()
+    {
+        return JsonResponse::create($this->pullRequestRepository->findAll());
     }
 }
